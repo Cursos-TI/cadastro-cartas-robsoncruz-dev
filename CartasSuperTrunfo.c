c8856df06152bfa9295a5bcca049e2f0625958cc
Robson Cruz Pimentel <robson_pimentel@proton.me>
2025-10-28 19:55:00 -0300

    Add input and display for city properties
    Desafio - Criando as Cartas do Super Trunfo
    Nivel Novato - Criar um programa em C que permita ao usuário inserir os dados de duas cartas do Super Trunfo.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,9 +7,74 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
+    int populacao_A, p_turistico_A, populacao_B, p_turistico_B;
+    char estado_A[5], cidade_A[30], cod_carta_A[5], estado_B[5], cidade_B[30], cod_carta_B[5];
+    float area_A, pib_A, area_B, pib_B;
+
   // Área para entrada de dados
 
-  // Área para exibição dos dados da cidade
+    printf("Insira a LETRA do Primeiro Estado: ");
+    scanf( " %s", &estado_A);
+
+    printf ("Insira o NÚMERO do Estado:  ");
+    scanf (" %s", &cod_carta_A);
+
+    printf("Insira o NOME da Cidade:  ");
+    scanf(" %s", &cidade_A);
+
+    printf("Insira a POPULAÇAO da Cidade: ");
+    scanf("%d", &populacao_A);
+
+    printf("Qual a ÁREA em Km2: ");
+    scanf("%f", &area_A);
+
+    printf("Qual o PIB em MBRL: ");
+    scanf(" %f", &pib_A);
+
+    printf("Quantidade de PONTOS TURÍSTICOS: ");
+    scanf("%d", &p_turistico_A);
+
+    printf(" -------------------------------- \n");
+
+    printf("Insira a LETRA do Segundo Estado: ");
+    scanf( " %s", &estado_B);
+
+    printf ("Insira o NÚMERO do Estado:  ");
+    scanf (" %s", &cod_carta_B);
+
+    printf("Insira o NOME da Cidade:  ");
+    scanf(" %s", &cidade_B);
+
+    printf("Insira a POPULAÇAO da Cidade: ");
+    scanf("%d", &populacao_B);
+
+    printf("Qual a ÁREA em Km2: ");
+    scanf("%f", &area_B);
+
+    printf("Qual o PIB em MBRL: ");
+    scanf("%f", &pib_B);
+
+    printf("Quantidade de PONTOS TURÍSTICOS: ");
+    scanf("%d", &p_turistico_B);
+
+    // Área para exibição dos dados da cidade
+  
+    printf(" ======= CARTA 1 ============ \n");
+    printf("Carta 1: %s \n", estado_A);
+    printf("Estado: %s \n",cod_carta_A);
+    printf("Nome da Cidade: %s\n", cidade_A);
+    printf("População: %d \n", populacao_A);
+    printf("Área: %f km2 \n", area_A);
+    printf("PIB: %f Milhoes de Reais \n", pib_A);
+    printf("Pontos Turísticos: %d \n", p_turistico_A);
+    printf("======== CARTA 2 ============ \n");
+    printf("Carta 1: %s \n", estado_B);
+    printf("Estado: %s \n",cod_carta_B);
+    printf("Nome da Cidade: %s\n", cidade_B);
+    printf("População: %d \n", populacao_B);
+    printf("Área: %f km2 \n", area_B);
+    printf("PIB: %f Milhoes de Reais \n", pib_B);
+    printf("Pontos Turísticos: %d \n", p_turistico_B);
 
 return 0;
 } 
